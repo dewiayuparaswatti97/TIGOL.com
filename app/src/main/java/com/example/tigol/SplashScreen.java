6a099e3e8921d2d327b658b01347c838c2637ce3
dewiayu97 <dewiayuparaswati97@gmail.com>
2019-02-26 15:43:08 +0700

    connect splash screen by rachma

--- app/src/main/java/com/example/tigol/SplashScreen.java
+++ app/src/main/java/com/example/tigol/SplashScreen.java
@@ -1,13 +1,25 @@
 package com.example.tigol;
 
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class SplashScreen extends AppCompatActivity {
+    public static int splash_timer_out=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, LoginPage.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },splash_timer_out);
     }
 }
