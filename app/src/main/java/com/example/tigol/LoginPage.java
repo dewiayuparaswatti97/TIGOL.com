0cf64dc87469f9418a0a510e3d542344e3d78b7c
yusaliano <sevenforce21@gmail.com>
2019-03-19 13:45:13 +0700

    onClick2

--- app/src/main/java/com/example/tigol/LoginPage.java
+++ app/src/main/java/com/example/tigol/LoginPage.java
@@ -1,7 +1,9 @@
 package com.example.tigol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,7 +11,7 @@ import android.widget.TextView;
 public class LoginPage extends AppCompatActivity {
 
     private EditText enterEmail;
-    private EditText enterUserName;
+    private EditText enterPassword;
     private Button btnLogin;
 
 
@@ -19,10 +21,15 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-        enterEmail = findViewById(R.id.edt_emailsign);
-        enterUserName = findViewById(R.id.edt_passsign);
-        btnLogin = findViewById(R.id.btn_signin);
+        enterEmail = findViewById(R.id.edt_emaillogin);
+        enterPassword = findViewById(R.id.edt_passlogin);
+        btnLogin = findViewById(R.id.btn_login);
 
-
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(LoginPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
