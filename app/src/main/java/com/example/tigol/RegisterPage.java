9e892373cbb341dc2219607f2de78ea326ad1b48
yusaliano <sevenforce21@gmail.com>
2019-03-21 15:25:23 +0700

    firebase login + regist + touchup main

--- app/src/main/java/com/example/tigol/RegisterPage.java
+++ app/src/main/java/com/example/tigol/RegisterPage.java
@@ -1,27 +1,98 @@
 package com.example.tigol;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterPage extends AppCompatActivity {
 
-        private EditText regsEmail;
-        private EditText regsPassword;
-        private EditText regsPasswordConf;
-        private Button btnCreate;
+    private EditText regsNama;
+    private EditText regsEmail;
+    private EditText regsPassword;
+    private EditText regsPasswordConf;
+    private Button btnCreate;
+    private FirebaseAuth frAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
+        regsNama = findViewById(R.id.edt_nameregist);
         regsEmail = findViewById(R.id.edt_emailregist);
         regsPasswordConf = findViewById(R.id.edt_passconfregist);
         regsPassword = findViewById(R.id.edt_passregist);
         btnCreate = findViewById(R.id.btn_createregist);
+
+        //firebase
+        frAuth = FirebaseAuth.getInstance();
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                doRegister();
+            }
+        });
     }
 
+    void doRegister() {
+        if (!validate())
+            return;
 
+        frAuth.createUserWithEmailAndPassword(regsEmail.getText().toString(), regsPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = frAuth.getCurrentUser();
+                            UserProfileChangeRequest builderUser = new UserProfileChangeRequest.Builder().setDisplayName(regsNama.getText().toString()).build();
+                            user.updateProfile(builderUser);
+                            Toast.makeText(RegisterPage.this, "Register berhasil!", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(RegisterPage.this, "Registerasi gagal : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean validate() {
+        boolean isValid = true;
+        if (regsEmail.getText().toString().equals("")) {
+            regsEmail.setError("Email kosong, tolong diisi.");
+            isValid = false;
+        }
+        if (regsNama.getText().toString().equals("")) {
+            regsNama.setError("Nama kosong, tolong diisi.");
+            isValid = false;
+        }
+        if (regsPassword.getText().toString().equals("")) {
+            regsPassword.setError("Password kosong, tolong diisi.");
+            isValid = false;
+        }
+        if (regsPasswordConf.getText().toString().equals("")) {
+            regsPasswordConf.setError("Password Confirm kosong, tolong diisi.");
+            isValid = false;
+        }
+        if (!regsPassword.getText().toString().equals(regsPasswordConf.getText().toString())) {
+            regsPasswordConf.setError("Password dan Confirmation password salah");
+            regsPasswordConf.requestFocus();
+            isValid = false;
+        }
+
+        return isValid;
+    }
 }
--- app/src/main/java/com/example/tigol/SplashScreen.java
+++ app/src/main/java/com/example/tigol/SplashScreen.java
@@ -6,20 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashScreen extends AppCompatActivity {
-    public static int splash_timer_out=3000;
+
+    public static int splash_timer_out = 3000;
+    private FirebaseAuth frAuth;
+    private Intent splashIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //firebase
+        frAuth = FirebaseAuth.getInstance();
+
+        //autologin / login kalo g ada akun yang masuk
+        if (frAuth.getCurrentUser() != null) {
+            splashIntent = new Intent(SplashScreen.this, MainActivity.class);
+        } else {
+            splashIntent = new Intent(SplashScreen.this, LoginPage.class);
+        }
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashScreen.this, LoginPage.class);
-                startActivity(homeIntent);
+                startActivity(splashIntent);
                 finish();
             }
-        },splash_timer_out);
+        }, splash_timer_out);
     }
 }
