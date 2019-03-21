d261207223d4ee6977c427ee314b251ce9958fec
yusaliano <sevenforce21@gmail.com>
2019-03-21 15:36:48 +0700

    sign-out buat main

--- app/src/main/java/com/example/tigol/MainActivity.java
+++ app/src/main/java/com/example/tigol/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.tigol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogoutMain;
+    private Button btnYourticket;
+    private FirebaseAuth frAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frAuth = FirebaseAuth.getInstance();
+        btnLogoutMain = findViewById(R.id.btn_logoutmain);
+
+        btnLogoutMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //firebase signout
+                frAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                Toast.makeText(MainActivity.this, "Sign-out Berhasil!", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
 
     }
 }
