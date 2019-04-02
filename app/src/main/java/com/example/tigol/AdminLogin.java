22548205c9bd63f04fd3d18807cf5e379b5a7d63
rachmawdy <rachmawidya7@gmail.com>
2019-04-02 13:07:51 +0600

    push activity admin login

--- app/src/main/java/com/example/tigol/AdminLogin.java
+++ app/src/main/java/com/example/tigol/AdminLogin.java
@@ -2,12 +2,23 @@ package com.example.tigol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AdminLogin extends AppCompatActivity {
+
+    private EditText enterEmail;
+    private EditText enterUserName;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        enterEmail = findViewById(R.id.edit_emailsign);
+        enterUserName = findViewById(R.id.edit_passsign);
+        btnLogin = findViewById(R.id.btn_signin);
+
     }
 }
