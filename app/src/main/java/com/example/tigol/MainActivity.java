ad898a4394a434c4be665f32771a18b3f5f44df4
rachmawdy <rachmawidya7@gmail.com>
2019-04-16 10:34:50 +0600

    menambahkan setting

--- app/src/main/java/com/example/tigol/MainActivity.java
+++ app/src/main/java/com/example/tigol/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tigol;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +9,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupSharedPreferences();
         setContentView(R.layout.activity_main);
 
     }
 }
+
+    private void setupSharedPreferences() {
+        SharedPreferences prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        toggleTheme(prefs.getBoolean("nightMode",false));
+
+    }
+
+    public void toggleTheme(Boolean bo){
+        if (bo){
+            setTheme(R.style.dark);
+        }else{
+            setTheme(R.style.light);
+        }
+
+    }
